2dc4c3ae7cc4637d534cd437a74a1fcd064f6491
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-08-07 17:44:43 +0530

    removed dist from git

--- dist/Functions/OTP/generateOtp.js
+++
@@ -1,9 +0,0 @@
-import otpGenerator from 'otp-generator';
-const otp = otpGenerator.generate(6, {
-    digits: true,
-    upperCaseAlphabets: false,
-    specialChars: false,
-    lowerCaseAlphabets: false
-});
-console.log(otp);
-export default otp;
--- dist/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-// supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
-const SUPABASE_URL = 'https://ikqijayghnkpfrwsjaji.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrcWlqYXlnaG5rcGZyd3NqYWppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA0NDQ2MDIsImV4cCI6MjAzNjAyMDYwMn0.RseAuJbInYGnayW4No3D5QAz54RwTeIYltWcfC44jXk';
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-export default supabase;
