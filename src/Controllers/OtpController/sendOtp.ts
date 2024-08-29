9d64df6c8f05f3c6a267465ff3ca51c0a437d151
Tejas Mehetre <142738639+Tejas-Mehetre@users.noreply.github.com>
2024-08-29 16:32:06 +0530

    bug fixes

--- src/Controllers/OtpController/sendOtp.ts
+++ src/Controllers/OtpController/sendOtp.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express';
-import sendOTP from '../../Functions/OTP/sendOtp.js';
-import generateOtp from '../../Functions/OTP/generateOtp.js';
-import OtpModel from '../../Models/AuthModels/otpModel.js'
+import { Request, Response } from "express";
+import sendOTP from "../../Functions/OTP/sendOtp.js";
+import generateOtp from "../../Functions/OTP/generateOtp.js";
+import OtpModel from "../../Models/AuthModels/otpModel.js";
 
 // import { error } from 'console';
 
@@ -9,25 +9,32 @@ interface SendOtpRequestBody {
   mb_no: string;
 }
 
-const sendOtp = async (req: Request<{}, {}, SendOtpRequestBody>, res: Response) => {
+const sendOtp = async (
+  req: Request<{}, {}, SendOtpRequestBody>,
+  res: Response
+) => {
   try {
     const { mb_no } = req.body;
-    
-    if(!mb_no){
-      return res.status(500).json({ error: "Mobile no is required to send otp"});
+
+    if (!mb_no) {
+      return res
+        .status(500)
+        .json({ error: "Mobile no is required to send otp" });
     }
-    const msg = 123456;
+    const otp = 123456;
     const newOtpRegistration = new OtpModel({
       mb_no,
-      otp : msg,
+      otp,
     });
 
     await newOtpRegistration.save();
 
-    return res.status(200).json({ msg: "OTP sent successfully" });
+    return res.status(200).json({ msg: "OTP sent successfully", otp });
   } catch (error) {
-    console.error('Error sending OTP:', error);
-    return res.status(500).json({ msg: "Failed to send OTP", error: error.message });
+    console.error("Error sending OTP:", error);
+    return res
+      .status(500)
+      .json({ msg: "Failed to send OTP", error: error.message });
   }
 };
 
